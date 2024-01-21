49e0091ac890ded3dbaf4abc5841c057c2c55134
DKDEV <dkgamelan@gmail.com>
2024-01-21 00:55:09 -0300

    Atualizado 20/01/2024 v-10

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Home } from './pages/home';
 import { NavBar } from './pages/nav';
 import { Sobre } from './pages/sobre';
@@ -5,22 +6,22 @@ import { LanguageProvider } from './context/Context';
 import { Projetos } from './pages/projetos';
 import { Contato } from './pages/contatos';
 
-
 const App = () => {
-
-
   return (
     <LanguageProvider>
-      <main id='home' className='h-screen w-screen' >
-        <NavBar />
-        <Home />
-        <Sobre />
-        <Projetos/>
-        <Contato/>
-      </main>
+      <BrowserRouter>
+        <div>
+          <NavBar />
+          <Routes>
+            <Route path="/" Component={Home} />
+            <Route path="/sobre" Component={Sobre} />
+            <Route path="/projetos" Component={Projetos} />
+            <Route path="/contato" Component={Contato} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </LanguageProvider>
-
-  )
+  );
 };
 
 export default App;
